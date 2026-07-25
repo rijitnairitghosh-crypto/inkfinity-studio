08fbdf682fb1e062fa8225bbb89518d05bc9ce6f
rijitnairitghosh-crypto <rijitnairitghosh@gmail.com>
2026-07-25 21:21:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.querySelector("button").addEventListener("click", function () {
-    alert("Welcome to INKFINITY STUDIO!");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const button = document.querySelector("button");
+
+    button.addEventListener("click", function () {
+        alert("Welcome to INKFINITY STUDIO! 🚀");
+    });
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        card.addEventListener("click", function () {
+            alert("This category will be available soon!");
+        });
+    });
+
 });
